301e1911c0eb473448b19310c72d242faf3a2964
hussien-m <hussienalfeqawy@gmail.com>
2023-01-11 18:09:10 +0200

    Add User Module

--- resources/views/dashboard/users/pagination_data.blade.php
+++ resources/views/dashboard/users/pagination_data.blade.php
@@ -1,33 +1,41 @@
-@foreach($users as $key=>$row)
-<tr>
- <td>{{ $users->firstItem() +$key }}</td>
- <td>{{ $row->first_name.' '.$row->last_name  }}</td>
- <td>{!! ($row->status==1) ? '<div class="badge bg-success p-1">مفعل</div>' :'<div class="badge bg-danger p-1">غير مفعل</div>'  !!}</td>
- <td>{{ $row->email }}</td>
- <td>{{ $row->created_at }}</td>
+@if ($users->count() > 0)
+    @foreach ($users as $key => $row)
+        <tr>
 
- <td>
-    <div class="btn-group btn-group-md">
-        <a href="" class="btn btn-primary">
-            <i class="fa fa-edit"></i>
-        </a>
+            <td>{{ $users->firstItem() + $key }}</td>
+            <td>{{ $row->first_name . ' ' . $row->last_name }}</td>
+            <td>{!! $row->status == 1
+                ? '<div class="badge bg-success p-1">مفعل</div>'
+                : '<div class="badge bg-danger p-1">غير مفعل</div>' !!}</td>
+            <td>{{ $row->email }}</td>
+            <td>{{ $row->created_at }}</td>
 
-        <a href="" class="btn btn-success">
-            <i class="fa fa-eye"></i>
-        </a>
-        <a href="javascript:void(0);" onclick="" class="btn btn-danger">
-            <i class="fa fa-trash"></i>
-        </a>
-    </div>
-    <form action="" method="post" id="" class="d-none">
-        @csrf
-        @method('DELETE')
-    </form>
-</td>
-</tr>
-@endforeach
+            <td>
+                <div class="btn-group btn-group-md">
+                    <a href="" class="btn btn-primary">
+                        <i class="fa fa-edit"></i>
+                    </a>
+
+                    <a href="" class="btn btn-success">
+                        <i class="fa fa-eye"></i>
+                    </a>
+                    <a href="javascript:void(0);" onclick="" class="btn btn-danger">
+                        <i class="fa fa-trash"></i>
+                    </a>
+                </div>
+                <form action="" method="post" id="" class="d-none">
+                    @csrf
+                    @method('DELETE')
+                </form>
+            </td>
+
+        </tr>
+    @endforeach
+@else
+    <td colspan="6" align="center">لايوجد نتائج</td>
+@endif
 <tr>
- <td colspan="3" align="center">
-  {!! $users->links() !!}
- </td>
+    @if ($users->count() > 0)
+        <td colspan="3" align="center"> {!! $users->links() !!}</td>
+    @endif
 </tr>
